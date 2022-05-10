a97294efc39b65639dd3201457b8802b7e9e8eaf
MomDra <yem9087@naver.com>
2022-05-10 23:51:34 +0900

    Interact UI - StartScene

---
+++ Safety Training/Assets/Scripts/UI/UIManager_StartScene.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager_StartScene : MonoBehaviour
+{
+    [SerializeField] GameObject playWindow;
+    [SerializeField] GameObject exitWindow;
+    [SerializeField] GameObject respondWindow;
+
+
+    public void ActivatePlayWindow(){
+        DeActiveAllWindow();
+        playWindow.SetActive(true);
+    }
+
+    public void ActiveExitWindow(){
+        DeActiveAllWindow();
+        exitWindow.SetActive(true);
+    }
+
+    public void ActivateRespondWindow(){
+        DeActiveAllWindow();
+        respondWindow.SetActive(true);
+    }
+
+    public void DeActiveAllWindow(){
+        playWindow.SetActive(false);
+        exitWindow.SetActive(false);
+        respondWindow.SetActive(false);
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("test_SiYoung");
+    }
+
+    public void EndGame(){
+        Application.Quit();
+    }
+}
