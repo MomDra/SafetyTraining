49adfe9bb6d7db788dc3b8134a289e956b1e3100
MomDra <yem9087@naver.com>
2022-05-13 22:20:21 +0900

    Warn siren

--- Safety Training/Assets/Scripts/GameManager/EmergencyManager.cs
+++ Safety Training/Assets/Scripts/GameManager/EmergencyManager.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class EmergencyManager : MonoBehaviour
 {
     [SerializeField] Light[] lights;
+    AudioSource audioSource;
+    int n;
 
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
 
 
     public void EmergencyStart(){
         StartCoroutine(ColorCoroutine());
+        audioSource.Play();
     }
 
     IEnumerator ColorCoroutine(){
+        n = 0;
 
         while(true){
 
             for(int i = 0; i < lights.Length; i++){
                 lights[i].color = Color.red;
+                lights[i].intensity = 5f;
             }
 
             yield return new WaitForSeconds(0.5f);
 
             for(int i = 0; i < lights.Length; i++){
                 lights[i].color = Color.white;
+                lights[i].intensity = 1f;
             }
 
             yield return new WaitForSeconds(0.5f);
+
+            n++;
+
+            if(n >= 11) break;
         }
         
     }
