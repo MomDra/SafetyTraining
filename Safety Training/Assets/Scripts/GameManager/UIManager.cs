4722cff889086e996c04499f79cad0d2746d75f5
MomDra <yem9087@naver.com>
2022-05-22 20:47:31 +0900

    Connect reason and accident UI

--- Safety Training/Assets/Scripts/GameManager/UIManager.cs
+++ Safety Training/Assets/Scripts/GameManager/UIManager.cs
@@ -27,6 +27,7 @@ public class UIManager
         this.reasonText = reasonText;
         this.accidentImg = accidentImg;
         this.accidentDescription = accidentDescription;
+        this.accidentScale = accidentScale;
         this.accidentConutermeasure = accidentConutermeasure;
 
         GameManager.Instance.ConncetUIManagerAndTaskManager(ref tasks, ref taskSolved);
@@ -52,7 +53,8 @@ public class UIManager
                 texts[2].color = Color.red;
             }
 
-            // newButton.GetComponent<Button>().onClick.AddListener(MakeReasonAndAccident, tasks[i].UI_INFO);
+            UI_Object info = tasks[i].UI_INFO;
+            newButton.GetComponent<Button>().onClick.AddListener(() => MakeReasonAndAccident(info));
         }
     }
 
@@ -72,15 +74,62 @@ public class UIManager
                 texts[2].text = "X";
                 texts[2].color = Color.red;
             }
+
+            UI_Object info = addedByPlayerList[i];
+            newButton.GetComponent<Button>().onClick.AddListener(() => MakeReasonAndAccident(info));
         }
     }
 
     public void AddUI(ref UI_Object UI_INFO){
         addedByPlayerList.Add(UI_INFO);
-        
     }
 
     void MakeReasonAndAccident(UI_Object UI_INFO){
+        if(UI_INFO.ReasonImg == null){
+            reasonImg.sprite = null;
+            reasonImg.color = Color.clear;
+        }
+        else{
+            reasonImg.sprite = UI_INFO.ReasonImg;
+            reasonImg.color = Color.white;
+        }
 
+        if(UI_INFO.ResasonText.Equals("")){
+            reasonText.text = "";
+        }
+        else{
+            reasonText.text = UI_INFO.ResasonText;
+        }
+
+        if(UI_INFO.AccidentImg == null){
+            accidentImg.sprite = null;
+            accidentImg.color = Color.clear;
+        }
+        else{
+            accidentImg.sprite = UI_INFO.AccidentImg;
+            accidentImg.color = Color.white;
+        }
+
+        if(UI_INFO.AccidentDescription.Equals("")){
+            accidentDescription.text = "";
+        }
+        else{
+            accidentDescription.text = UI_INFO.AccidentDescription;
+        }
+
+        if(UI_INFO.AccidentScale.Equals("")){
+            accidentScale.text = "";
+        }
+        else{
+            accidentScale.text = UI_INFO.AccidentScale;
+        }
+
+        if(UI_INFO.AccidentCountermeasure.Equals("")){
+            accidentConutermeasure.text = "";
+        }
+        else{
+            accidentConutermeasure.text = UI_INFO.AccidentCountermeasure;
+        }
+        
     }
 }
