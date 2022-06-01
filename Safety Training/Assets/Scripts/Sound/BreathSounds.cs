9fb4f749a749521c0f7bd8aa286ddea9b39632db
swy0123 <61828877+swy0123@users.noreply.github.com>
2022-06-01 22:14:43 +0900

    leakSounds -> BreathSounds

--- Safety Training/Assets/Scripts/Sound/BreathSounds.cs
+++ Safety Training/Assets/Scripts/Sound/BreathSounds.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeakSounds : MonoBehaviour
+public class BreathSounds : MonoBehaviour
 {
     AudioSource audioSource;
     [SerializeField]
