47336ddeeacd58e470b43839b9a664fa49d583f9
MomDra <yem9087@naver.com>
2022-06-03 22:37:48 +0900

    Update TableCollision.cs

--- Safety Training/Assets/Scripts/Tutorial/TableCollision.cs
+++ Safety Training/Assets/Scripts/Tutorial/TableCollision.cs
@@ -7,19 +7,27 @@ public class TableCollision : MonoBehaviour
     TutorialManager manager;
     [SerializeField]
     GameObject extinguisherHandle;
+    
+    bool trigger;
 
     private void Awake() {
         manager = FindObjectOfType<TutorialManager>();
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.layer == LayerMask.NameToLayer("Bottle")){
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Bottle") && !trigger){
+            trigger = true;
+
             manager.PlayAudioAndText(9);
 
             if(extinguisherHandle.GetComponent<OutlineOnly>() == null){
                 OutlineOnly outline = extinguisherHandle.gameObject.AddComponent<OutlineOnly>();
                 outline.OutlineColor = Color.magenta;
                 outline.OutlineMode = OutlineOnly.Mode.OutlineAll;
+            }
+
+            if(GetComponent<OutlineOnly>() != null){
+                Destroy(GetComponent<OutlineOnly>());
             }
         }
     }
