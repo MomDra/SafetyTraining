252f446e84f8e956a39fecace6206058370f3395
MomDra <yem9087@naver.com>
2022-06-04 00:11:23 +0900

    Update ExtinguiserHandleGrabEvent.cs

--- Safety Training/Assets/Scripts/Tutorial/ExtinguiserHandleGrabEvent.cs
+++ Safety Training/Assets/Scripts/Tutorial/ExtinguiserHandleGrabEvent.cs
@@ -7,24 +7,31 @@ public class ExtinguiserHandleGrabEvent : MonoBehaviour
     TutorialManager manager;
     PutCorrectionGrabable grabable;
 
+    Transform hoseHandle;
+
     private void Awake() {
         grabable = GetComponent<PutCorrectionGrabable>();
 
         grabable.grabBeginEvent.AddListener(GrabBegin);
+
+        hoseHandle = transform.Find("HozeHandle");
     }
 
     void GrabBegin(){
         if(manager.Index == 10){
             manager.PlayAudioAndText(10);
             
-            /*
+            
             // 호스에 아웃라인
-            if(lid.GetComponent<OutlineOnly>() == null){
-                OutlineOnly outline =  lid.gameObject.AddComponent<OutlineOnly>();
+            if(hoseHandle.GetComponent<OutlineOnly>() == null){
+                OutlineOnly outline =  hoseHandle.gameObject.AddComponent<OutlineOnly>();
                 outline.OutlineColor = Color.magenta;
                 outline.OutlineMode = OutlineOnly.Mode.OutlineAll;
             }
-            */
+            
+            if(GetComponent<OutlineOnly>() != null){
+                Destroy(GetComponent<OutlineOnly>());
+            }
         }
     }
 }
